c5ae53c76fc31eb478daa53441037888bf371bcd
rostyslav <rostyslav.kozel@smissltd.com>
2024-01-22 09:45:20 +0200

    async/await

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,32 @@
 const modal = document.getElementById("movieInfoModal");
 
-
 window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 };
 
-const showMovieInfo = (movieId) => {
-    fetch("/movie-info/" + movieId)
-        .then((response) => response.json())
-        .then((movie) => {
-            const movieInfoContent = `
-                <div class="p-6">
-                    <h3 class="text-xl font-semibold mb-4">${movie.title}</h3>
-                    <p class="text-md mb-2"><span class="font-bold">Year:</span> ${
-                        movie.release_year
-                    }</p>
-                    <p class="text-md mb-2"><span class="font-bold">Format:</span> ${
-                        movie.format
-                    }</p>
-                    <p class="text-md"><span class="font-bold">Actors:</span> ${movie.actors.join(
-                        ", "
-                    )}</p>
-                </div>
-            `;
+const showMovieInfo = async (movieId) => {
+    try {
+        const response = await fetch("/movie-info/" + movieId);
+        const movie = await response.json();
 
-            document.getElementById("movieInfoContent").innerHTML = movieInfoContent;
-            modal.style.display = "block";
-        });
+        const movieInfoContent = `
+            <div class="p-6">
+                <h3 class="text-xl font-semibold mb-4">${movie.title}</h3>
+                <p class="text-md mb-2"><span class="font-bold">Year:</span> ${movie.release_year}</p>
+                <p class="text-md mb-2"><span class="font-bold">Format:</span> ${movie.format}</p>
+                <p class="text-md"><span class="font-bold">Actors:</span> ${movie.actors.join(", ")}</p>
+            </div>
+        `;
+
+        document.getElementById("movieInfoContent").innerHTML = movieInfoContent;
+        modal.style.display = "block";
+    } catch (error) {
+        console.error("Failed to fetch movie info:", error);
+    }
 };
+
 
 const showModal = (modalId) => {
     const modal = document.getElementById(modalId);
